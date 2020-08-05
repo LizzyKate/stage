d4a97a63a7cd736a3480b678fe9e303066f3f6c5
unknown <onyenekwelizabeth@gmail.com>
2020-08-04 19:07:53 -0700

    animation

--- js/app.js
+++ js/app.js
@@ -4,51 +4,77 @@ import Img2 from "../img/Group 7.png"
 import Img3 from "../img/Group_8.png"
 import Img4 from "../img/Group_9.png"
 import Img5 from "../img/Group 10.png"
-import Img6 from "../img/Group 6.png"
 import img7 from "../img/Group_7.png"
 import Img8 from "../img/Group 5.png"
 
 
 // close modal
 export const closeModalButton = () => {
-    modal.classList.replace("display-show","display-none")
-    
+        modal.classList.remove("fade-in")
+        modal.children[0].classList.remove('scale-up-ver-center')
+        
+        modal.classList.add('fade-out')
+        modal.children[0].classList.add('scale-down-ver-center')
+    // modal.classList.toggle('scale-in-ver-top')
+    // modal.classList.toggle('scale-out-ver-top')
+    // // modal.classList.replace("display-show")
+    setTimeout(() => {
+        modal.classList.add("display-none")
+    }, 500)
 }
 
 // open modal
 export const openModalButton = () => {
-    modal.classList.replace("display-none", "display-show")
+    modal.classList.remove('fade-out')
+    modal.children[0].classList.remove('scale-down-ver-center')
+    modal.classList.remove("display-none")
+    modal.classList.add("fade-in")
+    modal.children[0].classList.add('scale-up-ver-center')
 }
 
 // open openImageModal
 export const openImageModal = (e, i) => {
     let menu = document.querySelector('.menu')
 
-    // if(e.target.parentElement.classList.contains("order2")){
+    
+
         const div = document.createElement("div")
         div.classList.add('back')
         
-        if( i === 0 ){
+        // if( i === 0 ){
             div.innerHTML = `
-            <div class="front">
-                <img src="${img7}" alt="">
+            <div class="front fade-in">
+                <img src="${img7}"  class="scale-in-modal">
             </div>
             `
-        }else{
-            div.innerHTML = `
-            <div class="front">
-                <img src="${data[i].image}" alt="">
-            </div>`
-        }
-        
+        // }else{
+        //     div.innerHTML = `
+        //     <div class="front fade-in">
+        //         <img src="${data[i].image}"  class='scale-in-modal'>
+        //     </div>`
+        // }
+
         menu.appendChild(div)
 
         div.addEventListener("click", e => {
-            if(!e.target?.src?.length){
-                div.style.display = 'none'
+            
+            if(!e.target?.src ){
+                e.target.children[0].classList.toggle('scale-in-modal')
+                e.target.children[0].classList.toggle('scale-out-modal')
+                // div.classList.toggle('fade-in')
+                
+                // div.classList.toggle('fade-out')
+                e.target.classList.toggle('fade-in')  
+                  
+                e.target.classList.toggle('fade-out') 
+                e.target.classList.toggle('front')
+                setTimeout(()=> {
+                    div.classList.toggle('back')
+                    div.style.display = 'none'
+                }, 1000)
+                  
             }
         })
-    // }
 }
 
 // data structure
